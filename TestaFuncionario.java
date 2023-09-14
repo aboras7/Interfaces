67da9e83d75c9664a87e9d0e7e1ef878ccb47ec4
Eduardo Nogueira <94703120+aboras7@users.noreply.github.com>
2023-09-14 12:57:08 -0300

    Add files via upload
    Adicionado o try/catch

--- TestaFuncionario.java
+++ TestaFuncionario.java
@@ -1,11 +1,22 @@
 package interfaces;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TestaFuncionario {
     public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
 
-        Gerente gerente1 = new Gerente("Robson", 42);
+        System.out.println("Digite o nome do funcionário ");
+        String nomeFun = leia.nextLine();
+
+        System.out.println("Digite a idade: ");
+        int idadeFun = Integer.parseInt(leia.nextLine());
+
+        Gerente gerente1 = new Gerente(nomeFun, idadeFun);
 
         System.out.println("Atributos de um Gerente: ");
+        System.out.println(gerente1.getNome() + "," + gerente1.getIdade());
         gerente1.abrirLoja();
         gerente1.vender();
         gerente1.almocar();
@@ -19,6 +30,18 @@ public class TestaFuncionario {
         vendedor1.vender();
         vendedor1.almocar();
         vendedor1.organizarCabides();
+
+        try {
+            int valorCaixa;
+            System.out.println("Digite o valor final do caixa: ");
+            valorCaixa = leia.nextInt();
+            System.out.println("Valor final do caixa: " + valorCaixa);
+
+        } catch (InputMismatchException e) {
+            System.err.println("\nExceção: " + e);
+            System.out.println("Digite um número inteiro: ");
+        }
         vendedor1.fecharLoja();
     }
 }
+
